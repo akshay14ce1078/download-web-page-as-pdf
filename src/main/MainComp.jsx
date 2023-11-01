adfd5ae71560674ab1db49f54bdff66f1359cc54
akshay14ce1078 <deoghare.akshay.14ce1078@gmail.com>
2023-11-01 20:12:57 +0530

    first wokring example

--- src/main/MainComp.jsx
+++ src/main/MainComp.jsx
@@ -1,8 +1,9 @@
-import { ReactLogoSVG } from "./ReactLogoSVG"
+import { ReactLogoSVG } from "./ReactLogoSVG";
+
 
 export const MainComp = () => {
   return  (<>
     <ReactLogoSVG />
-    <svg><rect width="300" height="100" style={{fill:'green',strokeWidth:3,stroke:'white'}}/></svg>
+    <svg><rect width="400" height="100" style={{fill:'green',strokeWidth:3,stroke:'white'}}/></svg>
   </>)
 }
