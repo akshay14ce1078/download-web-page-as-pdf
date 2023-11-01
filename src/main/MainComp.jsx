5e5fac62879e11d1eda6b6458c1519030effded7
akshay14ce1078 <deoghare.akshay.14ce1078@gmail.com>
2023-11-02 01:34:50 +0530

    feat: can convert and download complex chart also in pdf

--- src/main/MainComp.jsx
+++ src/main/MainComp.jsx
@@ -1,9 +1,9 @@
-import { ReactLogoSVG } from "./ReactLogoSVG";
-
+import { HighChartsStock } from "./HighChartsStock";
+// import { ReactLogoSVG } from "./ReactLogoSVG";
 
 export const MainComp = () => {
   return  (<>
-    <ReactLogoSVG />
+    <HighChartsStock />
     <svg><rect width="400" height="100" style={{fill:'green',strokeWidth:3,stroke:'white'}}/></svg>
   </>)
 }
